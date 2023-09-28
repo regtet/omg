ea8dab5266b1d3a822e9c2e425dd240b6542fdb0
灰太狼大王 <1827205006@qq.com>
2023-09-28 11:00:34 +0800

    绚丽的开关

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<style lang='scss' scoped>
+#App,
+body,
+html {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+
+<template>
+  <div id="App">
+    <router-view />
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, ref, reactive, toRefs, computed } from "vue";
+export default defineComponent({
+  components: {},
+  setup() {
+    return {};
+  }
+});
+</script>
